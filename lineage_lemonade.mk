5b966455d27a6591a7d1432e51800818ac82c49c
Bruno Martins <bgcngm@gmail.com>
2022-04-05 13:32:11 +0000

    lemonade: Update to Android 12 blobs
    Change-Id: Ib0843bfc2e802af16853875ae4649c403898f51f

--- lineage_lemonade.mk
+++ lineage_lemonade.mk
@@ -38,6 +38,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=OnePlus9 \
     TARGET_PRODUCT=OnePlus9_EEA \
-    PRIVATE_BUILD_DESC="OnePlus9-user 11   release-keys"
+    PRIVATE_BUILD_DESC="OnePlus9-user 12   release-keys"
 
-BUILD_FINGERPRINT := OnePlus/OnePlus9_EEA/OnePlus9:11/RKQ1.201105.002/2111112053:user/release-keys
+BUILD_FINGERPRINT := OnePlus/OnePlus9_EEA/OnePlus9:12/SKQ1.210216.001/R.202203102003:user/release-keys
