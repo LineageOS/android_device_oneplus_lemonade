dc4a0b50c0c9b6d9fcf50992bd64eafc917c8fdd
Bruno Martins <bgcngm@gmail.com>
2024-08-29 23:11:38 +0100

    lemonade: Enable LiveDisplay AntiFlicker feature
    Change-Id: I9d7da0d7b1551045f190391105164c9ebedbb683

--- device.mk
+++ device.mk
@@ -48,6 +48,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# LiveDisplay
+$(call soong_config_set,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_AF,true)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
